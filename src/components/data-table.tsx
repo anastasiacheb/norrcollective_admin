d711fa71baa1be6c39a30a2e4d2abf8d1493c802
Anastasia <161876455+anastasiacheb@users.noreply.github.com>
2026-08-07 19:06:37 +0300

    orders table visual

--- src/components/data-table.tsx
+++ src/components/data-table.tsx
@@ -21,10 +21,11 @@ import {
 } from "@/components/ui/table"
 
 import { DataTablePagination } from "./data-table-pagination"
+
+import { Card } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import {
   Select,
   SelectContent,
@@ -39,6 +40,23 @@ import {
   DropdownMenuContent,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+
+import { HugeiconsIcon } from "@hugeicons/react"
+import {
+  DragDropVerticalIcon,
+  CheckmarkCircle01Icon,
+  Loading03Icon,
+  MoreVerticalCircle01Icon,
+  LeftToRightListBulletIcon,
+  ArrowDown01Icon,
+  Add01Icon,
+  ArrowLeftDoubleIcon,
+  ArrowLeft01Icon,
+  ArrowRight01Icon,
+  ArrowRightDoubleIcon,
+  ArrowUpDownIcon,
+  ChartUpIcon,
+} from "@hugeicons/core-free-icons"
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
@@ -86,51 +104,66 @@ export function DataTable<TData, TValue>({
   }
 
   return (
-    <div>
-      <div className="flex items-center py-4">
-        <Input
-          placeholder="Search order..."
-          value={
-            (table.getColumn("orderNumber")?.getFilterValue() as string) ?? ""
-          }
-          onChange={(event) =>
-            table.getColumn("orderNumber")?.setFilterValue(event.target.value)
-          }
-          className="max-w-64 min-w-40"
-        />
-        <Tabs
-          value={status}
-          onValueChange={handleStatusChange}
-          className="w-full flex-col justify-start gap-6"
-        >
-          <TabsList className="hidden **:data-[slot=badge]:size-5 **:data-[slot=badge]:rounded-full **:data-[slot=badge]:bg-muted-foreground/30 **:data-[slot=badge]:px-1 @4xl/main:flex">
-            <TabsTrigger value="all">All</TabsTrigger>
-            <TabsTrigger value="pending">Pending</TabsTrigger>
-            <TabsTrigger value="processing">Processing</TabsTrigger>
-            <TabsTrigger value="shipped">Shipped</TabsTrigger>
-          </TabsList>
-        </Tabs>
-        <Select value={status} onValueChange={handleStatusChange}>
-          <SelectTrigger
-            className="flex w-fit @4xl/main:hidden"
-            size="sm"
-            id="view-selector"
+    <Card className="mx-4 px-4 lg:mx-6 lg:px-6">
+      <div className="flex items-center justify-between gap-2">
+        <div className="flex items-center gap-2">
+          <Input
+            placeholder="Search order..."
+            value={
+              (table.getColumn("orderNumber")?.getFilterValue() as string) ?? ""
+            }
+            onChange={(event) =>
+              table.getColumn("orderNumber")?.setFilterValue(event.target.value)
+            }
+            className="max-w-64 min-w-40"
+          />
+
+          <Select value={status} onValueChange={handleStatusChange}>
+            <SelectTrigger
+              className="flex w-fit @4xl/main:hidden"
+              size="sm"
+              id="view-selector"
+            >
+              <SelectValue placeholder="Select a view" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value="all">All</SelectItem>
+                <SelectItem value="pending">Pending</SelectItem>
+                <SelectItem value="processing">Processing</SelectItem>
+                <SelectItem value="shipped">Shipped</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+          <Tabs
+            value={status}
+            onValueChange={handleStatusChange}
+            className="w-full flex-col justify-start gap-6"
           >
-            <SelectValue placeholder="Select a view" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectItem value="all">All</SelectItem>
-              <SelectItem value="pending">Pending</SelectItem>
-              <SelectItem value="processing">Processing</SelectItem>
-              <SelectItem value="shipped">Shipped</SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
+            <TabsList className="hidden **:data-[slot=badge]:size-5 **:data-[slot=badge]:rounded-full **:data-[slot=badge]:bg-muted-foreground/30 **:data-[slot=badge]:px-1 @4xl/main:flex">
+              <TabsTrigger value="all">All</TabsTrigger>
+              <TabsTrigger value="pending">Pending</TabsTrigger>
+              <TabsTrigger value="processing">Processing</TabsTrigger>
+              <TabsTrigger value="shipped">Shipped</TabsTrigger>
+            </TabsList>
+          </Tabs>
+        </div>
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
-            <Button variant="outline" className="ml-auto">
-              Columns
+            <Button variant="outline" className="ml-auto" size="sm">
+              <HugeiconsIcon
+                icon={LeftToRightListBulletIcon}
+                strokeWidth={2}
+                data-icon="inline-start"
+              />
+              <span className="hidden xl:inline">Columns</span>
+
+              <HugeiconsIcon
+                icon={ArrowDown01Icon}
+                strokeWidth={2}
+                data-icon="inline-end"
+                className="hidden xl:inline"
+              />
             </Button>
           </DropdownMenuTrigger>
           <DropdownMenuContent align="end">
@@ -157,7 +190,7 @@ export function DataTable<TData, TValue>({
       </div>
       <div className="overflow-hidden rounded-2xl border">
         <Table>
-          <TableHeader>
+          <TableHeader className="sticky top-0 z-10 bg-muted">
             {table.getHeaderGroups().map((headerGroup) => (
               <TableRow key={headerGroup.id}>
                 {headerGroup.headers.map((header) => {
@@ -175,7 +208,7 @@ export function DataTable<TData, TValue>({
               </TableRow>
             ))}
           </TableHeader>
-          <TableBody>
+          <TableBody className="**:data-[slot=table-cell]:first:w-8 **:data-[slot=table-cell]:last:w-10">
             {table.getRowModel().rows?.length ? (
               table.getRowModel().rows.map((row) => (
                 <TableRow
@@ -185,7 +218,7 @@ export function DataTable<TData, TValue>({
                   {row.getVisibleCells().map((cell) => (
                     <TableCell
                       key={cell.id}
-                      className={cell.column.id === "actions" ? "w-10" : ""}
+                      // className={cell.column.id === "actions" ? "w-10" : ""}
                     >
                       {flexRender(
                         cell.column.columnDef.cell,
@@ -209,6 +242,6 @@ export function DataTable<TData, TValue>({
         </Table>
       </div>
       <DataTablePagination table={table} />
-    </div>
+    </Card>
   )
 }
